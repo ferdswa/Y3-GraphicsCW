4e273cd7fe70eb695130bddd0e488aa983c3269c
ferdswa <maximcarr03@hotmail.com>
2026-03-22 23:12:00 +0000

    -m: Speed up. TODO: Depth checking (still)

--- Assessment1A/Assessment1A/Assessment1A/rasterise.h
+++ Assessment1A/Assessment1A/Assessment1A/rasterise.h
@@ -29,15 +29,15 @@ void ApplyTransformationMatrix(glm::mat4 T, vector<triangle>& tris)//Split this 
 		tri.v3.pos = newPos;
 
 
-		tri.v1.nor[0] = tri.v1.pos[0];
-		tri.v1.nor[1] = tri.v1.pos[1];
-		tri.v1.nor[2] = tri.v1.pos[2];
-		tri.v2.nor[0] = tri.v2.pos[0];
-		tri.v2.nor[1] = tri.v2.pos[1];
-		tri.v2.nor[2] = tri.v2.pos[2];
-		tri.v3.nor[0] = tri.v3.pos[0];
-		tri.v3.nor[1] = tri.v3.pos[1];
-		tri.v3.nor[2] = tri.v3.pos[2];
+		tri.v1.nor.x = tri.v1.pos.x;
+		tri.v1.nor.y = tri.v1.pos.y;
+		tri.v1.nor.z = tri.v1.pos.z;
+		tri.v2.nor.x = tri.v2.pos.x;
+		tri.v2.nor.y = tri.v2.pos.y;
+		tri.v2.nor.z = tri.v2.pos.z;
+		tri.v3.nor.x = tri.v3.pos.x;
+		tri.v3.nor.y = tri.v3.pos.y;
+		tri.v3.nor.z = tri.v3.pos.z;
 	}
 }
 
@@ -46,33 +46,33 @@ void ApplyPerspectiveDivision(vector<triangle>& tris)
 	int w = 0;
 	for (triangle& tri : tris) {
 		//v1
-		w = tri.v1.pos[3];
-		tri.v1.pos[0] = tri.v1.pos[0] / w;
-		tri.v1.pos[1] = tri.v1.pos[1] / w;
-		tri.v1.pos[2] = tri.v1.pos[2] / w;
-		tri.v1.pos[3] = tri.v1.pos[3] / w;
+		w = tri.v1.pos.w;
+		tri.v1.pos.x = tri.v1.pos.x / w;
+		tri.v1.pos.y = tri.v1.pos.y / w;
+		tri.v1.pos.z = tri.v1.pos.z / w;
+		tri.v1.pos.w = tri.v1.pos.w / w;
 		//v2
-		w = tri.v2.pos[3];
-		tri.v2.pos[0] = tri.v2.pos[0] / w;
-		tri.v2.pos[1] = tri.v2.pos[1] / w;
-		tri.v2.pos[2] = tri.v2.pos[2] / w;
-		tri.v2.pos[3] = tri.v2.pos[3] / w;
+		w = tri.v2.pos.w;
+		tri.v2.pos.x = tri.v2.pos.x / w;
+		tri.v2.pos.y = tri.v2.pos.y / w;
+		tri.v2.pos.z = tri.v2.pos.z / w;
+		tri.v2.pos.w = tri.v2.pos.w / w;
 		//v3
-		w = tri.v3.pos[3];
-		tri.v3.pos[0] = tri.v3.pos[0] / w;
-		tri.v3.pos[1] = tri.v3.pos[1] / w;
-		tri.v3.pos[2] = tri.v3.pos[2] / w;
-		tri.v3.pos[3] = tri.v3.pos[3] / w;
+		w = tri.v3.pos.w;
+		tri.v3.pos.x = tri.v3.pos.x / w;
+		tri.v3.pos.y = tri.v3.pos.y / w;
+		tri.v3.pos.z = tri.v3.pos.z / w;
+		tri.v3.pos.w = tri.v3.pos.w / w;
 
-		tri.v1.nor[0] = tri.v1.pos[0];
-		tri.v1.nor[1] = tri.v1.pos[1];
-		tri.v1.nor[2] = tri.v1.pos[2];
-		tri.v2.nor[0] = tri.v2.pos[0];
-		tri.v2.nor[1] = tri.v2.pos[1];
-		tri.v2.nor[2] = tri.v2.pos[2];
-		tri.v3.nor[0] = tri.v3.pos[0];
-		tri.v3.nor[1] = tri.v3.pos[1];
-		tri.v3.nor[2] = tri.v3.pos[2];
+		tri.v1.nor.x = tri.v1.pos.x;
+		tri.v1.nor.y = tri.v1.pos.y;
+		tri.v1.nor.z = tri.v1.pos.z;
+		tri.v2.nor.x = tri.v2.pos.x;
+		tri.v2.nor.y = tri.v2.pos.y;
+		tri.v2.nor.z = tri.v2.pos.z;
+		tri.v3.nor.x = tri.v3.pos.x;
+		tri.v3.nor.y = tri.v3.pos.y;
+		tri.v3.nor.z = tri.v3.pos.z;
 	}
 }
 
@@ -83,21 +83,21 @@ void ApplyViewportTransformation(int w, int h, vector<triangle>& tris)
 		tri.v1.pos.x = (tri.v1.pos.x + 1) * (w/2.f);
 		tri.v1.pos.y = (tri.v1.pos.y + 1) * (h/2.f);
 		//v2
-		tri.v2.pos[0] = (tri.v2.pos[0] + 1) * (w / 2.f);
-		tri.v2.pos[1] = (tri.v2.pos[1] + 1) * (h / 2.f);
+		tri.v2.pos.x = (tri.v2.pos.x + 1) * (w / 2.f);
+		tri.v2.pos.y = (tri.v2.pos.y + 1) * (h / 2.f);
 		//v3
-		tri.v3.pos[0] = (tri.v3.pos[0] + 1) * (w / 2.f);
-		tri.v3.pos[1] = (tri.v3.pos[1] + 1) * (h / 2.f);
+		tri.v3.pos.x = (tri.v3.pos.x + 1) * (w / 2.f);
+		tri.v3.pos.y = (tri.v3.pos.y + 1) * (h / 2.f);
 
-		tri.v1.nor[0] = tri.v1.pos[0];
-		tri.v1.nor[1] = tri.v1.pos[1];
-		tri.v1.nor[2] = tri.v1.pos[2];
-		tri.v2.nor[0] = tri.v2.pos[0];
-		tri.v2.nor[1] = tri.v2.pos[1];
-		tri.v2.nor[2] = tri.v2.pos[2];
-		tri.v3.nor[0] = tri.v3.pos[0];
-		tri.v3.nor[1] = tri.v3.pos[1];
-		tri.v3.nor[2] = tri.v3.pos[2];
+		tri.v1.nor.x = tri.v1.pos.x;
+		tri.v1.nor.y = tri.v1.pos.y;
+		tri.v1.nor.z = tri.v1.pos.z;
+		tri.v2.nor.x = tri.v2.pos.x;
+		tri.v2.nor.y = tri.v2.pos.y;
+		tri.v2.nor.z = tri.v2.pos.z;
+		tri.v3.nor.x = tri.v3.pos.x;
+		tri.v3.nor.y = tri.v3.pos.y;
+		tri.v3.nor.z = tri.v3.pos.z;
 	}
 }
 
@@ -108,12 +108,12 @@ void ComputeBarycentricCoordinates(int px, int py, triangle t, float& alpha, flo
 	glm::vec4 b = t.v2.pos;
 	glm::vec4 c = t.v3.pos;
 
-	float lineABP = (b[1] - a[1]) * px + (a[0] - b[0]) * py + b[0] * a[1] - a[0] * b[1];
-	float lineBCA = (c[1] - b[1]) * a[0] + (b[0] - c[0]) * a[1] + c[0] * b[1] - b[0] * c[1];
-	float lineACP = (c[1] - a[1]) * px + (a[0] - c[0]) * py + c[0] * a[1] - a[0] * c[1];
-	float lineACB = (c[1] - a[1]) * b[0] + (a[0] - c[0]) * b[1] + c[0] * a[1] - a[0] * c[1];
-	float lineBCP = (c[1] - b[1]) * px + (b[0] - c[0]) * py + c[0] * b[1] - b[0] * c[1];
-	float lineABC = (b[1] - a[1]) * c[0] + (a[0] - b[0]) * c[1] + b[0] * a[1] - a[0] * b[1];
+	float lineABP = (b.y - a.y) * px + (a.x - b.x) * py + b.x * a.y - a.x * b.y;
+	float lineBCA = (c.y - b.y) * a.x + (b.x - c.x) * a.y + c.x * b.y - b.x * c.y;
+	float lineACP = (c.y - a.y) * px + (a.x - c.x) * py + c.x * a.y - a.x * c.y;
+	float lineACB = (c.y - a.y) * b.x + (a.x - c.x) * b.y + c.x * a.y - a.x * c.y;
+	float lineBCP = (c.y - b.y) * px + (b.x - c.x) * py + c.x * b.y - b.x * c.y;
+	float lineABC = (b.y - a.y) * c.x + (a.x - b.x) * c.y + b.x * a.y - a.x * b.y;
 
 	alpha = lineBCP / lineBCA;
 	beta = lineACP / lineACB;
@@ -122,16 +122,27 @@ void ComputeBarycentricCoordinates(int px, int py, triangle t, float& alpha, flo
 
 void ShadeFragment(triangle tri, float& alpha, float& beta, float& gamma, glm::vec3& col, float& depth)
 {
+	float z1, z2, z3;
 	if (0 <= alpha && 0 <= beta && 0 <= gamma)
 	{
 		if (1 >= alpha && 1 >= beta && 1 >= gamma) {
 
 			//If the furthest point on previous pixel is further than the furthest point on this triangle, render this triangle on top, otherwise, leave
 			
-			if (min({ tri.v1.pos[2], tri.v2.pos[2], tri.v3.pos[2] }) < depth)//Furthest point of triangle is closer than prev pixel depth
+
+			z1 = tri.v1.pos.z;
+			z2 = tri.v2.pos.z;
+			z3 = tri.v3.pos.z;
+
+			//if (z1 < depth || z2 < depth || z3 < depth) {
+			//	col = tri.v1.col;
+			//	depth = max({ tri.v1.pos.z, tri.v2.pos.z, tri.v3.pos.z });
+			//}
+
+			if (min({ tri.v1.pos.z, tri.v2.pos.z, tri.v3.pos.z }) < depth)//Closest point of triangle is closer than prev pixel depth
 			{
 				col = tri.v1.col;
-				depth = min({ tri.v1.pos[2], tri.v2.pos[2], tri.v3.pos[2] });
+				depth = min({ tri.v1.pos.z, tri.v2.pos.z, tri.v3.pos.z });
 			}
 		}
 	}
@@ -140,7 +151,8 @@ void ShadeFragment(triangle tri, float& alpha, float& beta, float& gamma, glm::v
 
 void Rasterise(vector<triangle> tris)
 {
-	float alpha, beta, gamma;
+	float alpha, beta, gamma, cDepth;
+	
 	for (int py = 0; py < PIXEL_H; py++)
 	{
 		float percf = (float)py / (float)PIXEL_H;
@@ -149,17 +161,18 @@ void Rasterise(vector<triangle> tris)
 
 		for (int px = 0; px < PIXEL_W; px++)
 		{
+			glm::vec3 col = glm::vec3(1.f);
 			for (triangle tri : tris) {
-				float cDepth = depth_buffer[py * PIXEL_W + px];
-				glm::vec3 col = glm::vec3(1.f);
+				cDepth = depth_buffer[py * PIXEL_W + px];
+				
 				ComputeBarycentricCoordinates(px, py, tri, alpha, beta, gamma);
 				ShadeFragment(tri, alpha, beta, gamma, col, cDepth);
 				if (col == tri.v1.col)
 				{
-					writeColToDisplayBuffer(col,px,py);
 					depth_buffer[py * PIXEL_W + px] = cDepth;
 				}
 			}
+			writeColToDisplayBuffer(col, px, py);
 		}
 	}
 	std::clog << "\rFinish rendering.           \n";
@@ -177,7 +190,7 @@ void render(vector<triangle>& tris)
 	ClearDepthBuffer();
 	//Apply transformations
 	modelMtx = glm::scale(modelMtx, glm::vec3(1.f, -1.f, 1.f));
-	viewMtx = glm::translate(viewMtx, glm::vec3(0.1, -2.5, -6.f));
+	viewMtx = glm::translate(viewMtx, glm::vec3(0.1, -2.5, -6));
 	projMtx = glm::perspective(glm::radians(55.f), (float)PIXEL_W / (float)PIXEL_H, 0.1f, 10.f);
 	
 	ApplyTransformationMatrix(viewMtx, tris);
